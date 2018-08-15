99a28ecd05e5a78a664ff33c0e86512a0d8755b5
Naufal Fikri <nauffik@gmail.com>
2018-08-16 00:07:45 +1000

    example of foreignkeys, see the twoFactorCode model

--- models/twoFactorCode.model.ts
+++ models/twoFactorCode.model.ts
@@ -1,9 +1,15 @@
 import * as Sequelize from 'sequelize';
+
+import Carer from "./carer.model";
+import Elder from "./elder.model";
 import Database from "../config/database.config";
 //not too sure about the expiry, could use DATETIME could use BOOLEAN
 const twoFactorCode = Database.define("twofactorcode", {
   code: Sequelize.STRING,
   expiry: Sequelize.DATETIME,
 });
+
+twoFactorCode.belongsTo(Carer);
+twoFactorCode.belongsTo(Elder);
 
 export default twoFactorCode;
