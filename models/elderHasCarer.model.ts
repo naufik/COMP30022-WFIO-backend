cb281aa510309c8461e6e693e4ecd326b50bfdbe
Naufal Fikri <nauffik@gmail.com>
2018-08-20 15:55:53 +1000

    super minor typo fix

--- models/elderHasCarer.model.ts
+++ models/elderHasCarer.model.ts
@@ -5,7 +5,7 @@ import Elder from "./elder.model";
 import Database from "../config/database.config";
 //not too sure about the expiry, could use DATETIME could use BOOLEAN
 const ElderHasCarer = Database.define("elderhascarer", {
-  establishedRelationship: sequelize.DATE,
+  establishedRelationship: Sequelize.DATE,
 });
 
 ElderHasCarer.belongsTo(Elder);
