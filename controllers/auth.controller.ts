a55f270740f9e5fa4fc26e5b8bdccda7e46f15b2
Naufal Fikri <nauffik@gmail.com>
2018-08-26 10:58:53 +1000

    added hash function for passwords

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,11 @@
 import * as Crypto from 'crypto';
+import AuthConfig from '../.serverconfig/auth';
 import Carer from '../models/carer.model';
 
 export class AuthController {
-
+    public static passHash(pass: string): string {
+        return Crypto.createHash("sha1")
+            .update(AuthConfig.preSalt(pass) + pass + AuthConfig.postSalt(pass))
+            .digest().toString();
+    }
 }
