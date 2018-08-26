25a65c09545330b3c62a432dfc9b6042e04f53f8
Naufal Fikri <nauffik@gmail.com>
2018-08-26 10:51:53 +1000

    added signup functionality that should work with postgresql database straight away

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,6 +1,26 @@
+import * as Bluebird from 'bluebird';
+
 import Carer from '../models/carer.model';
 import Elder from '../models/elder.model';
 
-export class UserController {
+import { NewUserData } from '../interfaces/user.interface';
 
+export default class UserController {
+    public static createUser(user: NewUserData): Bluebird<any> {
+        if (user.accountType == "CARER") {
+            return Carer.create({
+                email: user.email,
+                password: user.password,
+                fullName: user.fullName, 
+            });
+        } else if (user.accountType == "ELDER") {
+            return Elder.create({
+                email: user.email,
+                password: user.password,
+                fullName: user.fullName,
+            });
+        } else {
+           return Bluebird.reject(new Error("6001: Invalid account type given."));
+        }
+    }
 }
