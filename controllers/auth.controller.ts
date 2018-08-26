f262793093f92613ad7031843a052581630b235b
Naufal Fikri <nauffik@gmail.com>
2018-08-26 11:15:22 +1000

    fixed the controller not being default-exported

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import * as Crypto from 'crypto';
 import AuthConfig from '../.serverconfig/auth';
 import Carer from '../models/carer.model';
 
-export class AuthController {
+export default class AuthController {
     public static passHash(pass: string): string {
         return Crypto.createHash("sha1")
             .update(AuthConfig.preSalt(pass) + pass + AuthConfig.postSalt(pass))
