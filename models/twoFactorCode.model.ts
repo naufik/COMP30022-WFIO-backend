04ef3b7ad5ef99aae070d5bfe754c53753add246
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-31 19:13:58 +1000

    changed the twofactorcode code data type

--- models/twoFactorCode.model.ts
+++ models/twoFactorCode.model.ts
@@ -4,7 +4,7 @@ import Elder from "./elder.model";
 import Database from "../config/database.config";
 //not too sure about the expiry, could use DATETIME could use BOOLEAN
 const TwoFactorCode = Database.define("twofactorcode", {
-  code: Sequelize.STRING,
+  code: Sequelize.INTEGER,
   expiry: Sequelize.DATE
 });
 
