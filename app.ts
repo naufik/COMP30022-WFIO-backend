02623e28b638d9cc3d75fdbc951a985331b46df4
Naufal Fikri <nauffik@gmail.com>
2018-09-10 17:28:06 +1000

    imported (mock) user controllers properly

--- app.ts
+++ app.ts
@@ -1,17 +1,19 @@
 import * as Express from 'express';
 import * as CORS from 'cors';
+import UserRouter from './routes/user.router';
 
 const app: Express.Application = Express();
 
 app.use(CORS());
 
-app.listen(80, () => {
+app.listen(3000, () => {
     console.log("TEAM WE'LL FIGURE IT OUT");
     console.log("APPLICATION BACKEND");
     console.log("########################");
     console.log("RUNNING REVISION 1.0");
 });
 
+app.use('/user', UserRouter);
 app.get('/', (req, res) => {
     console.log("received new request");
     res.json({
--- routes/user.router.ts
+++ routes/user.router.ts
@@ -5,12 +5,21 @@ import { Action } from '../interfaces/action.interface';
 import { NewUser } from '../interfaces/user.interface';
 
 const UserRouter: Router = Router();
+const RouterFunctions: any = {}
 
 UserRouter.get('/', (req: Request, res: Response) => {
     res.send("Endpoint for Users works!");
 });
 
-UserRouter.post('/login', (req: Request, res: Response) => {
+UserRouter.post('/', (req: Request, res: Response) => {
+    let request = <Action<any>>req.body;
+
+    switch (request.action) {
+
+    }
+});
+
+RouterFunctions.SignUp = (req, res) => {
     let action: Action<any> = req.body;
 
     AuthController.login({
@@ -20,9 +29,9 @@ UserRouter.post('/login', (req: Request, res: Response) => {
     }).then((auth) => {
         res.json(auth);
     });
-});
+};
 
-UserRouter.post('/sign.up', (req: Request, res: Response) => {
+RouterFunctions.SignUp = (req: Request, res: Response) => {
     let action: Action<NewUser> = req.body;
 
     UserController.createUser(action.params)
@@ -37,4 +46,6 @@ UserRouter.post('/sign.up', (req: Request, res: Response) => {
         }).then((token) => {
             res.json(token);
         });
-});
+};
+
+export default UserRouter;
