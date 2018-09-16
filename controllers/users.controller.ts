fc3cff3b2fd6c0df56484618ada0dc39c73ea208
Naufal Fikri <nauffik@gmail.com>
2018-09-16 19:07:31 +1000

    getting user information now spits out their list of carers or elders, and their list of favorites as well. (still only as IDs)

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -2,6 +2,7 @@ import * as Bluebird from 'bluebird';
 
 import Carer from '../models/carer.model';
 import Elder from '../models/elder.model';
+import ElderHasCarer from '../models/elderHasCarer.model';
 import Favorites from '../models/favorites.model';
 import AuthController from './auth.controller';
 import { NewUser, User } from '../interfaces/user.interface';
@@ -31,7 +32,7 @@ export default class UserController {
         return returnedPromise;
     }
 
-    public static getUserByEmail(email: string): Bluebird<{ user: User, kind: string } | null> {
+    public static getUserByEmail(email: string): Bluebird<any | null> {
         return Bluebird.all([
             Carer.findOne({
                 where: {
@@ -53,22 +54,45 @@ export default class UserController {
                 user: userFound,
                 kind: (elderFound != null) ? "ELDER" : "CARER",
             };
+        }).then((userInfo: any) => {
+            if (userInfo != null) {
+                const user = userInfo.user.toJSON();
+                if (userInfo.kind === "ELDER") {
+                    // need to extract list of carers and list of favorite locations.
+                    return Bluebird.all([
+                        Favorites.findAll({
+                            where: {
+                                elderId: user.id
+                            }
+                        }),
+                        ElderHasCarer.findAll({
+                            where: {
+                                elderId: user.id
+                            }
+                        })
+                    ]).spread((favorites: any[], connections: any[]) => {
+                        user.favorites = favorites.map(thing => thing.toJSON());
+                        user.carersList = connections.map(thing => thing.toJSON().carerId);
+                        return user;
+                    });
+                } else if (userInfo.kind == "CARER") {
+                    // need to extract the list of elders this guy is taking care of
+                    return ElderHasCarer.findAll({
+                        where: {
+                            carerId: user.id
+                        }
+                    }).then((connections: any[]) => {
+                        user.eldersList = connections.map(thing => thing.toJSON());
+                        return user;
+                    });
+                } else {
+                    return Bluebird.reject(new Error("0000: Not Implemented"));
+                }
+            }              
         })
     }
 
     public static updateUser(user: User): Bluebird<any> {
-        return Bluebird.reject(new Error("0000: Not implemented."));
-    }
-
-    /**
-     * Need to add Methods that *READ* data here.
-     */
-
-    public static getElderData(id: string | number): Bluebird<User> {
-       return Bluebird.reject(new Error("0000: Not implemented."));
-    }
-
-    public static getCarerData(id: string | number): Bluebird<User> {
         return Bluebird.reject(new Error("0000: Not implemented."));
     }
 
