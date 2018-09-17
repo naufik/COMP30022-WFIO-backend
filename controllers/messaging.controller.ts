eb31890591aa5f345c0e3d05667e28c25278327c
Naufal Fikri <nauffik@gmail.com>
2018-09-18 06:14:37 +1000

    added proper method for sorting message in chronological order

--- controllers/messaging.controller.ts
+++ controllers/messaging.controller.ts
@@ -85,8 +85,19 @@ export default class MessagingController {
         msg.save();
       });
 
+
       return {
-        messages: sortedMessages.map(thing => thing.toJSON()),
+        messages: sortedMessages.map(thing => thing.toJSON()).sort((m, n) => {
+          let md: Date = new Date(m.timestamp);
+          let nd: Date = new Date(n.timestamp);
+          if (md > nd) {
+            return 1;
+          }
+          if (nd < md) {
+            return -1;
+          }
+          return 0;
+        }),
       };
     });
   }
