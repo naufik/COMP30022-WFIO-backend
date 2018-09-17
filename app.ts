952e8f80b73b293c6295df72a9a8d788bf4c7c78
Naufal Fikri <nauffik@gmail.com>
2018-09-17 18:28:10 +1000

    added real-time communication boilerplate for the server

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import * as Path from 'path';
 import * as HTTP from 'http';
 import * as HTTPS from 'https';
 import * as CORS from 'cors';
+import * as Socket from 'socket.io';
 import * as BodyParser from 'body-parser';
 import UserRouter from './routes/user.router';
 
@@ -27,14 +28,18 @@ app.get('/', (req, res) => {
     });
 });
 
-HTTP.createServer(app).listen(80, () => {
+const server1 = HTTP.createServer(app).listen(80, () => {
     console.log("[http] HTTP Server Running");
 });
 
-HTTP.createServer(app).listen(3000, () => {
+const server2 = HTTP.createServer(app).listen(3000, () => {
     console.log("[http] Legacy 3000 Server Running");
 });
 
-HTTPS.createServer(credentials, app).listen(443, () => {
+const server3 = HTTPS.createServer(credentials, app).listen(443, () => {
     console.log("[http] HTTPS Server Running");
 });
+
+const io1 = Socket(server1);
+const io2 = Socket(server2);
+const io3 = Socket(server3);
