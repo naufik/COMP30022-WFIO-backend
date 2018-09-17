2b8a01c261c0a3d70c3050f1ba7e5798eb94afed
Naufal Fikri <nauffik@gmail.com>
2018-09-17 20:19:04 +1000

    added message polling method

--- controllers/messaging.controller.ts
+++ controllers/messaging.controller.ts
@@ -3,8 +3,14 @@ import Elder from '../models/elder.model';
 import Connection from '../models/elderHasCarer.model';
 import Message from '../models/message.model';
 import UserController from './users.controller';
+import { User } from '../interfaces/user.interface';
+import ElderHasCarer from '../models/elderHasCarer.model';
+
+import { Op } from 'sequelize';
+import * as Bluebird from 'bluebird';
 
 export default class MessagingController {
+
   public static sendMessage(identity: string, params: any) {
     return UserController.getUserByEmail(identity).then((user) => {
       return Connection.findOne({
@@ -31,6 +37,47 @@ export default class MessagingController {
       });
     }).then((message: any) => {
       return message.toJSON();
+    });
+  }
+
+  public static pollMessages(identity: string, params: any) {
+    return UserController.getUserByEmail(identity).then((user: User) => {
+      const type = user.accountType;
+      
+      if (type === "ELDER") {
+        return ElderHasCarer.findAll({
+          where: {
+            elderId: user.id,
+          }
+        });
+      } else if (type === "CARER") {
+        return ElderHasCarer.findAll({
+          where: {
+            carerId: user.id,
+          }
+        })
+      } 
+      return Bluebird.reject(new Error("User Not Found"));
+    }).then((connections: any[]) => {
+
+      return Message.findAll({
+        where: {
+          [Op.or]: connections.map((thing) => {
+            return {
+              elderhascarerId: thing.toJSON().id,
+            }
+          }), 
+          polled: false,
+        }
+      });
+
+    }).then((messages: any[]) => {
+      return {
+        messages: messages.sort((t1: any, t2: any) => {
+          // compare by date later on.
+          return 1;  
+        })
+      };
     });
   }
 }
