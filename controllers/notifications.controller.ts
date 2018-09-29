9b3dc40d0e5861673dcc61aa9baf2b6891bba84c
Naufal Fikri <nauffik@gmail.com>
2018-09-29 17:56:39 +1000

    added notification buffer

--- controllers/notifications.controller.ts
+++ controllers/notifications.controller.ts
@@ -1,18 +1,30 @@
 import * as Bluebird from 'bluebird';
 import * as Socket from 'socket.io';
+import { Notification } from '../interfaces/notification.interface';
 
 export default class NotificationController {
-  private static notificationQueue: { to: string, action: {key: string, params: any }, content: string }[] = [];
+  private static notificationsQueue: Notification<any>[] = [];
 
-  public static addNotifications(recipient: string, message: string, action: { key: string, params: any }) {
-    this.notificationQueue.push({
-      to: recipient,
-      content: message,
-      action: action,
+  public static addNotification(notification: Notification<any>): Bluebird<any> {
+    this.notificationsQueue.push(notification);
+    return Bluebird.resolve(notification);
+  }
+
+  public static addNotifications(notifications: Notification<any>[]): Bluebird<any> {
+    this.notificationsQueue = notifications.concat(this.notificationsQueue);
+    return Bluebird.resolve(notifications);
+  }
+
+  public static pollNotifications(email: string): Bluebird<any> {
+    return new Bluebird((resolve, reject) => {
+      resolve({
+        notifications: this.notificationsQueue.filter(thing => thing.to === email)
+      });
+    }).then((notifs) => {
+      this.notificationsQueue = this.notificationsQueue.filter(thing => thing.to != email);
+      return notifs;
     });
   }
 
-  public static pollNotifications(identity: string, socket: Socket.Socket) {
-    return this.notificationQueue.filter(thing => thing.to === identity);
-  }
+
 }
